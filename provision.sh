491120f862edc287e21ac2d89b2feb1c0519847b
Tzanko Matev <tsanko@gmail.com>
2016-12-16 15:25:23 +0000

    Add sample express.js application
    The app should be installed automatically on machine provisioning. It
    can be run by calling
    
    $ survey

--- provision.sh
+++ provision.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-echo "import /vagrant/nixos/configuration.nix" >/etc/nixos/configuration.nix
+cat >/etc/nixos/vagrant.nix <<EOF
+# This file is overwritten by the vagrant-nixos plugin
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./vagrant-hostname.nix
+    ./vagrant-network.nix
+    /vagrant/nixos/configuration.nix
+    /vagrant/nixos/dev-tzanko.nix
+  ];
+}
+EOF
+
 nixos-rebuild switch
